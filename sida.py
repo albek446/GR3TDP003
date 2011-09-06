ff5e33e7624437690051e773759f158ff753c5ff
Alexander "Ace" Olofsson <ace@haxalot.com>
2011-09-06 13:59:26 +0200

    Testar templates, fungerar någolunda

--- sida.py
+++ sida.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ def hello():
 
 @app.route("/sida")
 def sida():
-  return "Working title goes here"
+  return render_template("layout.html")
   
 @app.route("/project/<int:id>")
 def getProj(id):
