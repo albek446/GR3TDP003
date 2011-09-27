c076b00305cdf2c1dded4f616a1d8217e2ad5821
Alexander "Ace" Olofsson <ace@haxalot.com>
2011-09-27 10:34:51 +0200

    Lekt med att implementera datalagret i hemsidan

--- sida.py
+++ sida.py
@@ -2,16 +2,21 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, abort
+from datalager import data
 
 app = Flask(__name__)
+data.init()
 
 def get_sidebar_projects():
-	toRet = []
+  toRet = []
 
-	for i in range(10):
-		toRet.append({"id": i, "title": "Projekt #" + str(i)})
+  err, asdf = data.retrieve_projects(sort_order="desc")
+  for i in range(len(asdf)):
+    toRet.append({"id": int(asdf[i]["project_no"]), "title": str(asdf[i]["project_name"])})
+    if i > 10:
+      break
 
-	return toRet
+  return toRet
 
 app.jinja_env.globals.update(get_sidebar_projects=get_sidebar_projects)
 
@@ -33,7 +38,7 @@ def search():
 
 @app.route("/project/<int:id>")
 def getProj(id):
-  return "Projekt med id #" + str(id) + " goes here!"
+  return render_template("project.html", project=data.lookup_project(id)[1])
 
 if __name__ == "__main__":
   app.run(debug = True, port=8080, host='0.0.0.0')
